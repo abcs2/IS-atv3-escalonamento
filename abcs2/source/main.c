9b7a3833a9b160be75a5925eae799dbee59a3794
abcs2 <abcs2@cesar.school>
2026-09-05 10:43:51 -0300

    mais logica e funcoes preenchidas

--- abcs2/source/main.c
+++ abcs2/source/main.c
@@ -17,7 +17,8 @@ typedef struct task {
 	int completeExecutionCount;
 	int killedCount;
 	
-	struct task *next;
+	struct task *nextList;
+    struct task *nextQueue;
 } Task;
 
 typedef struct {
@@ -26,24 +27,121 @@ typedef struct {
 
 typedef struct {
     Task *head;
-    Task *tail;
 } TaskQueue;
 
 
 
+void freeTasks(Task *task) {
+    if (task == NULL) {
+        return;
+    }
+    freeTasks(task->nextList);
+    free(task);
+    task = NULL;
+    
+    return;
+}
+
 void addTaskToList(TaskList *taskList, Task *task) {
+    Task *aux = taskList->head;
+    if (aux == NULL) {
+        taskList->head = task;
+        task->nextList = NULL;
+        return;
+    }
+    while (aux->nextList != NULL) {
+        aux = aux->nextList;
+    }
+    aux->nextList = task;
+    task->nextList = NULL;
+    return;
+}
+
+void createTask(TaskList *taskList, int period, int deadline, int burst) {
+    Task *task = (Task *) malloc(sizeof(Task));
+    if (task == NULL) {
+        return;
+    }
+    task->period = period;
+    task->deadline = deadline;
+    task->burst = burst;
+
+    task->timeFromStart = 0;
+    task->timeFromExec = 0;
+
+    task->lostDeadlinesCount = 0;
+    task->completeExecutionCount = 0;
+    task->killedCount = 0;
+
+    task->nextList = NULL;
+    task->nextQueue = NULL;
+    addTaskToList(taskList, task);
+
     return;
 }
 
 void addTaskToQueue(TaskQueue *taskQueue, Task *task) {
+    Task *aux = taskQueue->head;
+    if (aux == NULL) {
+        taskQueue->head = task;
+        task->nextQueue = NULL;
+        return;
+    }
+    while (aux->nextQueue != NULL) {
+        aux = aux->nextQueue;
+    }
+    aux->nextQueue = task;
+    task->nextQueue = NULL;
+
     return;
 }
 
-Task *getTaskFromQueue(TaskQueue *taskQueue, Task *task) {
-    return task;
+void removeTaskFromQueue(TaskQueue *taskQueue, Task *task) {
+    Task *aux = taskQueue->head;
+    if (aux == task) {
+        taskQueue->head = task->nextQueue;
+        return;
+    }
+    while (aux->nextQueue != task) {
+        if (aux->nextQueue == NULL) {
+            return;
+        }
+        aux = aux->nextQueue;
+    }
+    aux->nextQueue = task->nextQueue;
+
+    return;
 }
 
-void updateTaskQueue(TaskQueue *taskQueue) {
+void killRemainingTasks(TaskQueue *taskQueue) {
+    Task *aux = taskQueue->head;
+    while (aux != NULL) {
+        aux->killedCount++;
+        aux = aux->nextQueue;
+    }
+    
+    return;
+}
+
+void updateTaskQueue(TaskQueue *taskQueue, TaskList *taskList, int *totalTicks) {
+    Task *aux = taskQueue->head;
+    while (aux != NULL) {
+        if (aux->timeFromStart == aux->deadline) {
+            aux->lostDeadlinesCount++;
+            removeTaskFromQueue(taskQueue, aux);
+        }
+        aux = aux->nextQueue;
+    }
+    aux = taskList->head;
+    while (aux != NULL) {
+        if ((*totalTicks) % aux->period == 0) {
+            aux->timeFromStart = 0;
+            aux->timeFromExec = 0;
+            addTaskToQueue(taskQueue, aux);
+        }
+        aux = aux->nextList;
+    }
+
     return;
 }
 
@@ -57,13 +155,13 @@ Task *getMostPriority(TaskQueue *taskQueue, int maxTicks) {
             priority = aux;
             period = aux->period;
         }
-        aux = aux->next;
+        aux = aux->nextQueue;
     }
 
     return priority;
 }
 
-int waitForTask(TaskQueue *taskQueue, int *totalTicks, int maxTicks) {
+int waitForTask(TaskQueue *taskQueue, TaskList *taskList, int *totalTicks, int maxTicks) {
     int currentCounter = 0, code;
     while (1) {
         if ((*totalTicks) == maxTicks) {
@@ -79,73 +177,76 @@ int waitForTask(TaskQueue *taskQueue, int *totalTicks, int maxTicks) {
 
         (*totalTicks)++;
         currentCounter++;
-        updateTaskQueue(taskQueue);
+        updateTaskQueue(taskQueue, taskList, totalTicks);
     }
     if (currentCounter > 0) {
         printf("idle for %d units", currentCounter);
     }
+
     return code;
 }
 
-void randomBoringFunctionThatExistsToCallWaitForTaskBoringFunction(TaskQueue *taskQueue, int *totalTicks, int maxTicks) {
-    int code = waitForTask(taskQueue, totalTicks, maxTicks);
+void randomBoringFunctionThatExistsToCallWaitForTaskBoringFunction(TaskQueue *taskQueue, TaskList* taskList, int *totalTicks, int maxTicks) {
+    int code = waitForTask(taskQueue, taskList, totalTicks, maxTicks);
 
     if (code == 0) {
-        // kill remaining tasks
+        killRemainingTasks(taskQueue);
     } else if (code == 1) {
-        runTask(taskQueue, getMostPriority(taskQueue, maxTicks), totalTicks, maxTicks);
+        runTask(taskQueue, taskList, getMostPriority(taskQueue, maxTicks), totalTicks, maxTicks);
     }
+
+    return;
 }
 
 void updateTimeFromStart(TaskQueue *taskQueue) {
     Task *aux = taskQueue->head;
     while (aux != NULL) {
         aux->timeFromStart++;
-        aux = aux->next;
+        aux = aux->nextQueue;
     }
+
     return;
 }
 
-char getStatus(Task *task, int *totalTicks, int maxTicks) {
-    char status = ' ';
-    if (task->timeFromExec == task->burst) {
-		status = 'F';
-        task->completeExecutionCount++;
-	} else if (task->timeFromStart == task->deadline) {
-		status = 'L';
-        task->lostDeadlinesCount++;
-	} else if ((*totalTicks) == maxTicks) {
-		status = 'K';
-        task->killedCount++;
-	} else {
-		status = 'H';
-	}
-    return status;
-}
-
-void runTask(TaskQueue *taskQueue, Task *task, int *totalTicks, int maxTicks) {
+void runTask(TaskQueue *taskQueue, TaskList *taskList, Task *task, int *totalTicks, int maxTicks) {
 	int currentCounter = 0;
 	char status = ' ';
-	while ((task->timeFromStart < task->deadline) && (task->timeFromExec < task->burst) && ((*totalTicks) < maxTicks)) {
+	while (1) {
 		// execute task...
 		(*totalTicks)++;
-		updateTimeFromStart(taskQueue);
+        currentCounter++;
 		task->timeFromExec++;
-		currentCounter++;
+        updateTimeFromStart(taskQueue);
+        updateTaskQueue(taskQueue, taskList, totalTicks);
 
-        updateTaskQueue(taskQueue);
+        if (task->timeFromExec == task->burst) {
+            task->completeExecutionCount++;
+            removeTaskFromQueue(taskQueue, task);
+            status = 'F';
+            break;
+        }
+        if (task->timeFromStart == task->deadline) {
+            status = 'L';
+            break;
+        }
+        if ((*totalTicks) == maxTicks) {
+            status = 'K';
+            break;
+        }
         if (task != getMostPriority(taskQueue, maxTicks)) {
+            status = 'H';
             break;
         } 
 	}
     // execution interrupted or finished
-    status = getStatus(task, totalTicks, maxTicks);
-
+    task->timeFromExec = 0;
     printf("[%s] for %d units - %c\n", task->name, currentCounter, status);
+
     return;
 }
 
 int main(int argc, char **argv) {
+    // period > deadline > burst
 
     return 0;
 }
